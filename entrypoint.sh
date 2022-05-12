db9907af21f8cf24f7404794d4f1da400d329ebe
Ellen Carriere <ellencarriere@gmail.com>
2022-05-12 10:43:07 -0500

    fix names

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,8 @@ REPO_NAME=$(jq -r .event.base.repo.name /github/workflow/event.json)
 EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
 # Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
-app="${INPUT_NAME:-pr-$PR_NUMBER-$REPO_OWNER-$REPO_NAME}"
-postgres_app="${INPUT_POSTGRES_NAME:-pr-$PR_NUMBER-$REPO_OWNER-$REPO_NAME-postgres}"
+app="${INPUT_NAME:-pr-$PR_NUMBER}"
+postgres_app="${INPUT_POSTGRES_NAME:-pr-$PR_NUMBER-postgres}"
 region="${INPUT_REGION:-${FLY_REGION:-iad}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
