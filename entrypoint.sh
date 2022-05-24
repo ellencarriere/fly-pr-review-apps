6af3bac47f8e3cb56d05c3d4ad5b902c629830b7
Ellen Carriere <ellencarriere@gmail.com>
2022-05-24 12:43:05 -0500

    add memory as an input

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ postgres_app="${INPUT_POSTGRES:-$REPO_NAME-pr-$PR_NUMBER-postgres}"
 region="${INPUT_REGION:-${FLY_REGION:-ord}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
-memory="$MEMORY"
+memory="$INPUT_MEMORY"
 
 echo "MEM1"
 echo "$memory"
